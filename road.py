c3d441f1471c8ed7c1cd2888354de26e0ef05e48
yuto-0424 <75009595+yuto-0424@users.noreply.github.com>
2020-12-12 15:11:06 +0900

    Delete road.py

--- road.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import sys
-
-def road_info():
-    df = pd.read_html("https://roadway.yahoo.co.jp/list", index_col=0)[0]
-    result_road = df.to_csv(header=False)
-
-    return result_road
